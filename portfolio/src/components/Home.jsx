fe211fb838d8a0153537ff81a6cb54c2c1e7080a
CelsoHerib <102439750+CelsoHerib@users.noreply.github.com>
2025-06-13 23:53:09 -0300

    canbios de efectos

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -3,35 +3,54 @@ import styled from "styled-components";
 export default function Home() {
   return (
     <Header>
+      <img src="./src/images/photo.png" alt="photo" className="photo"/>
       <Content>
         <h1>Celso Herib Ortigoza Duarte</h1>
-        <h4>Full stack developer</h4>
+        <span>Full stack developer</span>
       </Content>
     </Header>
   );
 }
 
 const Header = styled.header`
-  color: white;
-  background: linear-gradient(
-      to bottom,
-      rgba(1, 1, 1, 0.7),
-      rgba(145, 139, 142, 0.7)
-    ),
-    url('./src/images/photo.png') no-repeat center center / cover; /* Centrado perfecto */
-  backdrop-filter: blur(1px);
-  min-height: 100vh;
   width: 100%;
+  height: 100vh;
   display: flex;
+  flex-direction: column; /* Cambio clave: elementos en columna */
   justify-content: center;
   align-items: center;
-  position: relative;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
+  padding-left: 10%;
+  
+  .photo {
+    width: 500px;
+    max-width: 90%; /* Para responsive */
+    margin-bottom: 2rem; /* Espacio entre imagen y texto */
+  }
+
+  img {
+    width: 100%;
+    display: block;
+    border-radius: 10px; /* Opcional: esquinas redondeadas */
+  }
 `;
 
 const Content = styled.div`
   text-align: center;
-  z-index: 1;
+  
+  h1 {
+    font-size: 4rem;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: var(--secunday-color);
+    -webkit-text-stroke: 1px white;
+    margin: 0 0 1rem 0;
+    color: var(--text-light);
+  }
+  
+  span {
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: var(--secundary-color);
+    -webkit-text-stroke: 1px white;
+  }
 `;
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,69 +1,81 @@
 import styled from "styled-components";
 
-
-export default function Navbar(){
-    return(
-        <NavBar>
-        <div class="sidebar">
-            <a href="#home"><img src="./src/images/logo.png" alt="logo" /></a>
-  <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
-  <a href="#about"><i class="fa fa-fw fa-address-card"></i> About</a>
-  <a href="#clients"><i class="fa fa-fw fa-user"></i> Testimonials</a>
-  <a href="#projects"><i class="fa fa-fw fa-folder"></i> Projects</a>
-  <a href="#skills"><i class="fa fa-fw fa-code"></i> Skills</a>
-  <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-</div>
-</NavBar>
-    )
+export default function Navbar() {
+  return (
+    <NavBar>
+      <div class="sidebar">
+        <a href="#home">
+          <img src="./src/images/logo.png" alt="logo" />
+        </a>
+        <a href="#home">
+          <i class="fa fa-fw fa-home"></i> Home
+        </a>
+        <a href="#about">
+          <i class="fa fa-fw fa-address-card"></i> About
+        </a>
+        <a href="#clients">
+          <i class="fa fa-fw fa-user"></i> Testimonials
+        </a>
+        <a href="#projects">
+          <i class="fa fa-fw fa-folder"></i> Projects
+        </a>
+        <a href="#skills">
+          <i class="fa fa-fw fa-code"></i> Skills
+        </a>
+        <a href="#contact">
+          <i class="fa fa-fw fa-envelope"></i> Contact
+        </a>
+      </div>
+    </NavBar>
+  );
 }
-
 
 const NavBar = styled.div`
-.sidebar {
-max-height: 100vh;
-overflow-y: auto;
-width: 160px;
-position: fixed;
-z-index: 1;
-  height: auto;
-  top: 0;
-  bottom: 0;
+  .sidebar {
+    max-height: 100vh;
+    overflow-y: auto;
+    width: 160px;
+    position: fixed;
+    z-index: 1;
+    height: auto;
+    top: 0;
+    bottom: 0;
+    background-color: #111;
+    padding-top: 36px;
+    transition: 0.5s;
+    font-size: 18px;
+    color: #f1f1f1;
+    font-weight: bold;
+    text-align: center;
+  }
 
-  background-color: #111;
-  padding-top: 36px;
-  transition: 0.5s;
-  font-size: 18px;
-  color: #f1f1f1;
-  font-weight: bold;
-  text-align: center;
-}
+  /* Style sidebar links */
+  .sidebar a {
+    padding: 26px 18px 26px 18px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #818181;
+    display: block;
+  }
 
-/* Style sidebar links */
-.sidebar a {
-  padding: 26px 18px 26px 18px;
-  text-decoration: none;
-  font-size: 20px;
-  color: #818181;
-  display: block;
-}
+  /* Style links on mouse-over */
+  .sidebar a:hover {
+    color: var(--text-hover);
+  }
 
-/* Style links on mouse-over */
-.sidebar a:hover {
-  color: #f1f1f1;
-}
+  /* Style the main content */
+  .main {
+    margin-left: 160px; /* Same as the width of the sidenav */
+    padding: 0px 10px;
+  }
 
-/* Style the main content */
-.main {
-  margin-left: 160px; /* Same as the width of the sidenav */
-  padding: 0px 10px;
-}
-
-/* Add media queries for small screens (when the height of the screen is less than 450px, add a smaller padding and font-size) */
-@media screen and (max-height: 450px) {
-  .sidebar {padding-top: 15px;}
-  .sidebar a {font-size: 18px;}
-}
-
-`
-
-
+  /* Add media queries for small screens (when the height of the screen is less than 450px, add a smaller padding and font-size) */
+  @media screen and (max-height: 450px) {
+    .sidebar {
+      padding-top: 15px;
+    }
+    .sidebar a {
+      font-size: 18px;
+    }
+  }
+`;
